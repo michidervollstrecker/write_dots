1c62df249d48c47a0b583f4c9421e4cd05175084
michi <bergen.michael@web.de>
2025-10-26 10:39:24 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number up to 15:");
+            int us = int.Parse(Console.ReadLine());
+            if (us > 15)
+            {
+                Console.WriteLine("Error: Number too large");
+                return;
+            }
+            string p = ".";
+            int i = 0;
+            string[] arr = new string[us];
+            while (i < us)
+            {
+                for (int x = 0; x < i; x++)
+                { p = p + ("."); };
+                arr[i] = arr[i] + p;
+                Console.WriteLine(arr[i]);
+                i++;
+            }
+        }
+    }
+}
+
+
+
+
